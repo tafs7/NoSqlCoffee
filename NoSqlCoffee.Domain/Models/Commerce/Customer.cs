4a5d75488ab31c68d23aa64de28d8e583973f0d0
Thiago Silva <tafs7@yahoo.com>
2016-04-11 17:16:29 -0500

    added entity base for inheritance of id, and removed unused using stmts

--- NoSqlCoffee.Domain/Models/Commerce/Customer.cs
+++ NoSqlCoffee.Domain/Models/Commerce/Customer.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NoSqlCoffee.Domain.Common;
 
 namespace NoSqlCoffee.Domain.Models.Commerce
 {
-    public class Customer
+    public class Customer : Entity
     {
     }
 }
--- NoSqlCoffee.Domain/Models/Commerce/User.cs
+++ NoSqlCoffee.Domain/Models/Commerce/User.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NoSqlCoffee.Domain.Common;
 
 namespace NoSqlCoffee.Domain.Models.Commerce
 {
-    public class User
+    public class User : Entity
     {
     }
 }
